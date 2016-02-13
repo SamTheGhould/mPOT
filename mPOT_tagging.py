f70218a15950d521e04b6b4b7b177c8d6d6214c9
SamTheGhoul <Steiner_samuel@protonmail.coml>
2016-02-12 23:10:55 -0500

    Version 1.00

--- mPOT_tagging.py
+++ mPOT_tagging.py
@@ -10,37 +10,73 @@ Description: A system which uses music files' (MP3, OGG, FLAC, and Wave) tags to
 """
 from tinytag import TinyTag
 from string import Template
+import tkFileDialog
 import os
+import unicodedata
+
 
 def meta_data_map(meta_data):
+    u = unicodedata.normalize
     m = meta_data
-    meta_data_dict = dict(album = m.album, artist = m.artist, audio_offset = m.audio_offset, bitrate = m.bitrate,
-                          disc = m.disc, disc_total = m.disk_total, duration = m.duration, filesize = m.filesize,
-                          genre = m.genre, samplerate = m.samplerate, title = m.title, track = m.track,
-                          track_total = m.track_total, year = m.year)
+    meta_data_dict = dict(album=u('NFKD', m.album).encode('ascii', 'ignore'),
+                          artist=u('NFKD', m.artist).encode('ascii', 'ignore'),
+                          audio_offset=m.audio_offset,
+                          bitrate=m.bitrate,
+                          disc=m.disc,
+                          duration=m.duration,
+                          filesize=m.filesize,
+                          genre=u('NFKD', m.genre).encode('ascii', 'ignore'),
+                          samplerate=m.samplerate,
+                          title=u('NFKD', m.title).encode('ascii', 'ignore'),
+                          track=m.track,
+                          track_total=m.track_total,
+                          year=m.year)
     return meta_data_dict
 
 
 def derive_name(meta_data):
-    try:
-        f = open('name', 'r')
-        name_temp = f.read()
-        file_name_temp = Template(name_temp)
-        key = meta_data_map(meta_data)
-        file_name = file_name_temp.substitute(key)
-        return file_name
-    except:
-        if os._exists('name'):
-            print("Unkown Error")
-        else:
-            print("Name file missing")
+    # print meta_data.artist
+    f = open('name', 'r')
+    name_temp = f.read()
+    file_name_temp = Template(name_temp)
+    key = meta_data_map(meta_data)
+    file_name = file_name_temp.substitute(key)
+    return file_name
+
 
 
 
 def move_mass(input_folder, output_folder):
     tag = TinyTag
-    for (a,b,c) in os.walk(input_folder):
+    for (a, b, c) in os.walk(input_folder):
         for x in c:
+            # print x
+            file_type = x.split('.')[1]
+            # print file_type
             song_loc = input_folder + "\\" + x
             file_meta = tag.get(song_loc)
             name = derive_name(file_meta)
+            artist = unicodedata.normalize('NFKD', file_meta.artist).encode('ascii','ignore')
+            album = unicodedata.normalize('NFKD', file_meta.album).encode('ascii','ignore')
+            out_path = output_folder + "\\" + artist + "\\" + album
+            # print name
+            final_name = out_path + "\\" + name + '.' + file_type
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+                if not os.path.isfile(final_name):
+                    os.rename(song_loc, out_path + "\\" + name + '.' + file_type)
+                else:
+                    os.remove(song_loc)
+            else:
+                if not os.path.isfile(final_name):
+                    os.rename(song_loc, out_path + "\\" + name + '.' + file_type)
+                else:
+                    os.remove(song_loc)
+
+
+def main():
+    input = tkFileDialog.askdirectory( initialdir="/", title="select unorganized media")
+    output = tkFileDialog.askdirectory( initialdir="/", title="select output")
+    move_mass(input, output)
+
+main()
